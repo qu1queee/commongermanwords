dc52925e5d890389c647d0e3d6432fb8092bb890
qu1queee <encalada@de.ibm.com>
2021-12-15 14:54:43 -0500

    Isolate constants
    Such as regex or well-known names

---
+++ src/pkg/goword/parser/aux.go
@@ -0,0 +1,31 @@
+package parser
+
+const (
+	//
+	// WIKTIONARY API URL's
+	URLHEAD = "https://de.wiktionary.org/w/api.php?action=parse&page="
+	URLTAIL = "&prop=wikitext&format=json"
+	//
+
+	//
+	// List of current REGEXP
+	JSONREGEXPARSER = `\"wikitext\":\{\"\*\":\"(.*?)\"\}\}\}$`
+	// https://regex101.com/r/G8C38F/1
+	LANGUAGETYPEPARSER = `(^={2}\s.*Sprache\|)(.*)(\}\}\)\s={2}$)`
+	// https://regex101.com/r/br7rzZ/1
+	BLOCKTYPEPARSER = `(^{\{)(.*)(\}\})|(^\s{\{)(.*)(\}\})`
+	//
+
+	DESIREDLANGUAGE = "Deutsch"
+
+	//
+	//List of categories(blocks) of interest
+	WORDTYPE      = "Wortart|"
+	SIEHEAUCH     = "Siehe auch|"
+	PRONUNCIATION = "Aussprache"
+	MEANING       = "Bedeutungen"
+	EXAMPLES      = "Beispiele"
+	TRANSLATION   = "Übersetzungen"
+	FEATURES      = "Grammatische Merkmale"
+	//
+)
